4b07f827b7ebf20bb3e8a2a7a1411842a45d1c3e
mdmas <mdmasudalam2349@gmail.com>
2024-01-14 02:54:13 +0600

    Kt

---
+++ app/src/main/java/com/example/kotlin/MYClass.kt
@@ -0,0 +1,75 @@
+package com.example.kotlin
+
+import android.animation.ValueAnimator
+
+/*
+
+ fun main()
+ {
+     val str="Hello my friends"
+     print(str+"\n\n")
+
+     print("hello")
+ }
+*/
+
+/*
+ class MYClass
+ {
+     companion object{
+         @JvmStatic
+
+         fun main(args :Array<String>)
+         {
+
+             println("hello my friend")
+             print("hello")
+         }
+     }
+
+ }*/
+
+
+class MYClass
+{
+    companion object
+
+    {
+
+        @JvmStatic
+        fun  main (args:Array<String>)
+        {
+
+           /* Int
+            String
+            Boolean
+            Float
+            Double
+            Long
+            Char
+            Byte
+            Short
+            var  a :Int=7
+            var name: String="Developer"
+
+
+            println("Hello")
+            println("Hello $a "+a)
+            println("Hello $name "+name)*/
+
+
+            var a=10
+            a=7
+            a=5
+            a=8
+            a=12
+            val b="MASUD"
+            //b="ALAM"
+
+            println("$a $b")
+        }
+
+
+    }
+
+}
